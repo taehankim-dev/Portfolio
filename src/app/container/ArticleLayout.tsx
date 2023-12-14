9121c8ee06f5712f72f79149ecff59f58ef741f1
TaeHan <goldmini95@gmail.com>
2023-12-14 16:43:55 +0900

    article layout에 id 값 추가

--- src/app/container/ArticleLayout.tsx
+++ src/app/container/ArticleLayout.tsx
@@ -11,7 +11,7 @@ export default function ArticleLayout({
   return (
     <>
       {
-        <article className={`${bgColorClass} pt-2 pb-6`}>
+        <article id={articleTitle} className={`${bgColorClass} pt-2 pb-6`}>
           <div className="p-4 xl:w-3/4 lg:w-5/6 md:w-full mx-auto">
             <p className="article-title text-center font-bold border-b-2 p-2 mb-4">{articleTitle}</p>
             <div className="article-inner">
