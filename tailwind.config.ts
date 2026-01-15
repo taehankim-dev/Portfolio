402cb9fa455fe78517904e1ab6bdc8fb44e6ae9c
TaeHan <goldmini95@gmail.com>
2026-01-15 15:24:35 +0900

    config type error 해결

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,26 +1,25 @@
-import type { Config } from 'tailwindcss'
+const config = {
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/lib/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/container/**/*.tsx'
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
 
-const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/lib/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/container/**/*.tsx'
-  ],
-  theme: {
-    extend: {
-      fontFamily:{
-        
-      }
-    }
-  },
-  safelist: [
-    {
-      pattern: /bg-(pink|yellow|amber|purple|slate)-(200|300|400)/,
-      variants: ['lg']
-    }
-  ],
-  plugins: [],
+            }
+        }
+    },
+    safelist: [
+        {
+            pattern: /bg-(pink|yellow|amber|purple|slate)-(200|300|400)/,
+            variants: ['lg']
+        }
+    ],
+    plugins: [],
 }
+
 export default config
